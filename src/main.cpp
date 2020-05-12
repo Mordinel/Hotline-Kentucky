9577cd62840893f9fbe062be632233ebdab1bae4
Mason <sorom2@student.op.ac.nz>
2020-05-13 00:20:42 +1200

    Delete redundant includes and function in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,9 @@
 // main.cpp
 #include <SFML/Graphics.hpp>
-#include <iostream>
-#include <vector>
-#include <string>
 
-#include "Player.h"
-#include "Dungeon.h"
-#include "Room.h"
-#include "TileType.h"
-#include "TileMap.h"
-#include "Gun.h"
-#include "Enemy.h"
-#include "EntityManager.h"
-#include "EnemyManager.h"
-#include "Item.h"
-#include "ItemType.h"
 #include "GameManager.h"
 
-#define ZOOM_INCREMENTS 0.2f
-#define ZOOM_MIN 0.6f
-#define ZOOM_MAX 100.0f
-#define ZOOM_DEFAULT 1.0f
-
 const unsigned int random_seed = static_cast<unsigned int>(time(NULL));
-const int tileSize = 32;
-
-void setViewZoom(float* viewZoom, int mouseDelta) {
-    if (mouseDelta < 0) {
-        *viewZoom += ZOOM_INCREMENTS;
-    } else if (mouseDelta > 0) {
-        *viewZoom -= ZOOM_INCREMENTS;
-    }
-
-    if (*viewZoom < ZOOM_MIN) {
-        *viewZoom = ZOOM_MIN;
-    } else if (*viewZoom > ZOOM_MAX) {
-        *viewZoom = ZOOM_MAX;
-    }
-}
 
 int main()
 {
