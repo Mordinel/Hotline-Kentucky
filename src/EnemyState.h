24e02aa5f29d34c913e2da4e25296e21a3bca8ce
Mason <sorom2@student.op.ac.nz>
2020-05-15 03:36:40 +1200

    Enemy state comments

--- src/EnemyState.h
+++ src/EnemyState.h
@@ -1,3 +1,11 @@
+/******************************************************************************
+ * filename: EnemyState.h
+ * filetype: c++ header file
+ * authors: james olsen (1000060387) & mason soroka-gill (1000049111)
+ * created on: 10/05/2020
+ * description: Enum that describes the statefulness of enemies.
+ *****************************************************************************/
+
 #ifndef ENEMYSTATE_H
 #define ENEMYSTATE_H
 
