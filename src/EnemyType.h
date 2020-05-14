112b90fdfa64623b958eccaa2f7cedb9a8d90cb8
James Olsen <jdwo@live.com>
2020-05-15 04:41:22 +1200

    added comments to enemy type and player state

--- src/EnemyType.h
+++ src/EnemyType.h
@@ -1,3 +1,11 @@
+/******************************************************************************
+ * Filename: EnemyType.h
+ * FileType: C++ Header File
+ * Authors: James Olsen (1000060387) & Mason Soroka-Gill (1000049111)
+ * Created On: 14/05/2020
+ * Description: An enum that describes what type of enemy an enemy is.
+ *****************************************************************************/
+
 #ifndef ENEMYTYPE_H
 #define ENEMYTYPE_H
 
--- src/PlayerState.h
+++ src/PlayerState.h
@@ -4,6 +4,7 @@
  * Authors: James Olsen (1000060387) & Mason Soroka-Gill (1000049111)
  * Created On: 14/05/2020
  * Description: An enum that describes the players current condition/state.
+ *              Used to implement the power ups logic plus know when the player is dead.
  *****************************************************************************/
 
 #ifndef PLAYERSTATE_H
