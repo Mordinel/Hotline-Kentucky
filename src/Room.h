e6600abac35a2a521ba23c95ce378bc88b63ab9a
Mason <sorom2@student.op.ac.nz>
2020-05-15 02:39:20 +1200

    Room comments

--- src/Room.h
+++ src/Room.h
@@ -1,3 +1,11 @@
+/******************************************************************************
+ * Filename: Room.h
+ * FileType: C++ Header File
+ * Authors: James Olsen (1000060387) & Mason Soroka-Gill (1000049111)
+ * Created On: 29/04/2020
+ * Description: Data structure for dungeon room generation process.
+ *****************************************************************************/
+
 #ifndef ROOM_H
 #define ROOM_H
 
