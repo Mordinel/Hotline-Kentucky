a35dd15184bc54ea0281302111dd77fbedbfc00c
James Olsen <jdwo@live.com>
2020-05-14 22:53:46 +1200

    added header comment to Box.h

--- src/Box.h
+++ src/Box.h
@@ -1,3 +1,15 @@
+/******************************************************************************
+ * Filename: Box.h
+ * FileType: C++ Source File
+ * Authors: James Olsen (1000060387) & Mason Soroka-Gill (1000049111)
+ * Created On: 06/05/2020
+ * Description: Simple struct that contains the positions of each side of a box or square shape. Useful for finding the bounds of an area.
+ *              Left being the left most x position.
+ *              Top  being the top most y position.
+ *              Right being the right most x position.
+ *              Bottom being the bottom most y position.
+ *****************************************************************************/
+
 #ifndef BOX_H
 #define BOX_H
 
