4e9e996a691888291a6df7a3c14b1766239182a8
James Olsen <jdwo@live.com>
2020-05-15 01:43:50 +1200

    added comments to Collider.h and Direction.h

--- src/Direction.h
+++ src/Direction.h
@@ -1,3 +1,11 @@
+/******************************************************************************
+ * Filename: Direction.h
+ * FileType: C++ Header File
+ * Authors: James Olsen (1000060387) & Mason Soroka-Gill (1000049111)
+ * Created On: 01/05/2020
+ * Description: Enum containing directions. Used by Dungeon class to randomly pick a direction for new rooms.
+ *****************************************************************************/
+
 #ifndef DIRECTION_H
 #define DIRECTION_H
 
