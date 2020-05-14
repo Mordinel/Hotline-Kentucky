dd1ce7588489ac470f7f6ac9f03bf1959c4e1fda
James Olsen <jdwo@live.com>
2020-05-14 14:09:16 +1200

    Added a player state enum and added it to the Player class

--- src/Player.h
+++ src/Player.h
@@ -4,15 +4,18 @@
 #include <SFML/Graphics.hpp>
 
 #include "Entity.h"
+#include "PlayerState.h"
 
 // define PLayer as child of entity and Collider
 class Player : public Entity {
 public:
     Player(sf::Texture* texture, sf::RenderWindow* window, sf::Vector2u imageCount, float switchTime, float speed, std::vector<std::vector<TileType>>* tileMap);
     void Update(float* deltaTime) override;
+    PlayerState GetState();
 
 private:
     sf::Vector2f lastMovement;
+    PlayerState state;
 };
 
 #endif
